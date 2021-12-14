a9aabc748d7ed2938ec8520868867c9343eebd69
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-14 09:48:19 +0100

    convert random to select

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const options = document.querySelector('.js_option');
 const button = document.querySelector('.js_button');
 const player = document.querySelector('.js_player');
 const computer = document.querySelector('.js_computer');
+const result = document.querySelector('.js_result')
 
 //Creo una variable para la opcion de la maquina, le paso un 10 para que me devuelva un valor del 1-10.
 let optionComputer = getRandomNumber(10);
@@ -13,19 +14,34 @@ let optionComputer = getRandomNumber(10);
 
 //Funciones
 
-
-
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
+
 function handleClick(event) {
     //Cuando pulse necesito recoger el valor selecionado
-    const resultSelected = options.value;
-    console.log(resultSelected);
+    const optionPlayer = options.value
+
+    //Convierto el valor aletorio al valor del seleccionable
+
+    if (optionComputer < 3) {
+        optionComputer = 1;
+    } else if (optionComputer >= 6) {
+        optionComputer = 2;
+    } else {
+        optionComputer = 3;
+    }
 
     //Cuando tenga el valor seleccionado tengo que compararlo con el valor generado aleatorio.
 
+
+
+    if (optionPlayer === optionComputer) {
+        result.innerHTML = "Empate";
+    } else if () {
+
+    }
 
     //Mostrar mensaje
 
