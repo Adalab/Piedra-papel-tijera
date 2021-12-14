abb6a1b8bf40f6e3516ee2061e0e500c283f87fd
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-14 09:18:44 +0100

    addEvent

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ const options = document.querySelector('.js_option');
 const button = document.querySelector('.js_button');
 const player = document.querySelector('.js_player');
 const computer = document.querySelector('.js_computer');
+
+//Funciones
+
+function handleClick(event) {
+
+
+}
+
+//Eventos
+button.addEventListener('click', handleClick);
