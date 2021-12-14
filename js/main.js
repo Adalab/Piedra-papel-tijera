12e3d96b6dd721c49dac61067bfbd041853ea611
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-14 09:31:08 +0100

    add GetRandom

--- js/main.js
+++ js/main.js
@@ -7,10 +7,29 @@ const button = document.querySelector('.js_button');
 const player = document.querySelector('.js_player');
 const computer = document.querySelector('.js_computer');
 
+//Creo una variable para la opcion de la maquina, le paso un 10 para que me devuelva un valor del 1-10.
+let optionComputer = getRandomNumber(10);
+
+
 //Funciones
 
-function handleClick(event) {
 
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+function handleClick(event) {
+    //Cuando pulse necesito recoger el valor selecionado
+    const resultSelected = options.value;
+    console.log(resultSelected);
+
+    //Cuando tenga el valor seleccionado tengo que compararlo con el valor generado aleatorio.
+
+
+    //Mostrar mensaje
+
+    //incrementar el contador
 
 }
 
