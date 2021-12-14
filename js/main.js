ad8190e5f18c2c88fcc58ff71f4d44559612fd34
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-14 09:15:14 +0100

    getElemnts

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//1.Recogemos los elementos
+
+const options = document.querySelector('.js_option');
+const button = document.querySelector('.js_button');
+const player = document.querySelector('.js_player');
+const computer = document.querySelector('.js_computer');
